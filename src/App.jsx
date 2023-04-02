9468d725b184ee9a95f3da17f13cce7af5ec371a
Siam T. Bhuiyan <siamtbhuiyan@gmail.com>
2023-04-02 08:27:46 +0000

    Color Palate Selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 const App = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto background-color">
       <h1 className="font-bold text-xl">AnyMed</h1>
+      <button className="primary-color px-3 py-2 rounded text-white">Button</button>
     </div>
   )
 }
