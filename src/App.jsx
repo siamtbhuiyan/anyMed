9c8aa0f3d6ff06849a9f539f5d2431f742cae486
Siam T. Bhuiyan <siamtbhuiyan@gmail.com>
2023-04-04 01:41:05 +0600

    Data configured and Navbar Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Navbar from "./components/Navbar"
 const App = () => {
+
   return (
-    <div className="container mx-auto background-color">
-      <h1 className="font-bold text-xl">AnyMed</h1>
-      <button className="primary-color px-3 py-2 rounded text-white">Button</button>
+    <div className="container mx-auto">
+      <Navbar />
     </div>
   )
 }
---
+++ src/hooks/index.js
@@ -0,0 +1,26 @@
+import { Search, ShoppingCartOutlined } from '@mui/icons-material'
+import stringSimilarity from 'string-similarity'
+
+function useIcon(word) {
+    const icons = {Search, ShoppingCartOutlined}
+    let available = false;
+    Object.keys(icons).map(i => {
+        if (i === word) {
+            available = true
+        }
+        return true
+    })
+    if (available) {
+        const iconsNames = Object.keys(icons)
+        let matches = stringSimilarity.findBestMatch(word, iconsNames)
+        const bestMatch = matches.bestMatch.target
+        const Icon = icons[bestMatch]
+        return Icon
+    } else {
+        const EmptyComp = () => {
+            return "";
+        }
+        return EmptyComp
+    }
+}
+export default useIcon
